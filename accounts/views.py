46de0595d33f98bd8f286a27309aaf47413f4f4e
Scott Rubin <apreche@frontrowcrew.com>
2009-06-30 08:46:22 -0400

    added view function for all accounts belonging to specific user

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,11 @@
-from django.views.generic.list_detail import object_detail
+from django.views.generic.list_detail import object_detail, object_list
 from accounts.models import Account
 
 def account_detail( request, slug ):
     accounts = Account.objects.all()
     return object_detail( request, accounts, slug=slug )
+
+@login_required
+def owned_account_list( request ):
+    accounts = Account.objects.filter(owner=request.user)
+    return object_list( request, accounts )
